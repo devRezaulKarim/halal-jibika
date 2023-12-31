2d078dd2c07eaac56cb28bfff5fea861baf5dcd3
devRezaulKarim <dev.rezaulkarim@gmail.com>
2023-12-31 08:14:34 +0600

    Footer done

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/SharedPages/Navbar";
+import Footer from "../pages/SharedPages/Footer";
 
 export default function MainLayout() {
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <div style={{ minHeight: "76.2vh" }}>
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 }
