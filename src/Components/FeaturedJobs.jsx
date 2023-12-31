42cc710b175f012bbf2917c03cf6fdfa36d3f40f
devRezaulKarim <dev.rezaulkarim@gmail.com>
2023-12-31 18:42:10 +0600

    show all button done

--- src/Components/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs.jsx
@@ -2,6 +2,7 @@ import useFetch from "../Hooks/useFetch";
 import classes from "../Styles/FeaturedJobs.module.css";
 import Spinner from "../Utls/Spinner";
 import ErrorMessage from "../pages/NothingFound/ErrorMessage";
+import Buttons from "./Buttons";
 import JobCard from "./JobCard";
 
 export default function FeaturedJobs() {
@@ -16,7 +17,7 @@ export default function FeaturedJobs() {
   return (
     <div>
       <div className={classes.featuredHeading}>
-        <h1>Featured Jobs</h1>
+        <h1>Latest Jobs</h1>
       </div>
       <div className={classes.featuredProducts}>
         {[...data]
@@ -25,6 +26,11 @@ export default function FeaturedJobs() {
           .map((d) => (
             <JobCard key={d.id} d={d} featured={true} />
           ))}
+      </div>
+      <div className={classes.showAllBtn}>
+        <div>
+          <Buttons to={"/jobs"}>Show All Jobs</Buttons>
+        </div>
       </div>
     </div>
   );
